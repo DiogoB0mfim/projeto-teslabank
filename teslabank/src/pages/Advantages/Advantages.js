9fa29643ca8f20ee0b3287111ae11ffa3d4a93ea
DiogoB0mfim <diogohbomfim@gmail.com>
2023-01-07 01:01:42 -0300

    update : pag vantagens terminada

--- teslabank/src/pages/Advantages/Advantages.js
+++ teslabank/src/pages/Advantages/Advantages.js
@@ -1,57 +1,59 @@
 import * as S from "../../styles/AdvantagesStyled"
+import card from "../../assets/credit-card.png"
+import clock from "../../assets/clock.png"
+import percent from "../../assets/percent.png"
+import gift from "../../assets/gift.png"
+import shield from "../../assets/shield.png"
+import cart from "../../assets/shopping-cart.png"
+
 
 const Advantages = () => {
     return (
-        <>
-            <div>
-                <div>
-                    <h2>Quais as Vantagens<br/> de usar <span>TeslaBank</span>?</h2>
-                    <div>
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-
-                        <div>
-                            <img src={""} alt=""/>
-                            <h3>Cartão TeslaBank</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</p>
-                            <a href="/">VEJA AS OPÇÕES</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+        <>  
+            <S.ContainerAdvantages>
+                <S.ContainerTitle>
+                    <S.HashTag>#INOVAÇÃO</S.HashTag>
+                    <S.Title>Quais as vantagens<br /> de usar <S.RedSpan>TeslaBank</S.RedSpan>?</S.Title>
+                </S.ContainerTitle>
+                <S.DivAllAdvantage>
+                    <S.EachAdvantage>
+                        <img src={card} alt="" />
+                        <S.H3Advantage>Cartão TeslaBank</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">VEJA AS OPÇÕES</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                    <S.EachAdvantage>
+                        <img src={cart} alt="" />
+                        <S.H3Advantage>Investimentos</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">LER MAIS</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                    <S.EachAdvantage>
+                        <img src={clock} alt="" />
+                        <S.H3Advantage>Atendimento 24h</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">VEJA AS OPÇÕES</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                    <S.EachAdvantage>
+                        <img src={shield} alt="" />
+                        <S.H3Advantage>Segurança</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">LER MAIS</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                    <S.EachAdvantage>
+                        <img src={percent} alt="" />
+                        <S.H3Advantage>Sem taxas</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">COMO FUNCIONA</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                    <S.EachAdvantage>
+                        <img src={gift} alt="" />
+                        <S.H3Advantage>Receba prêmios</S.H3Advantage>
+                        <S.PAdvantage>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo lacinia, tempus odio hendrerit, ultricies diam.</S.PAdvantage>
+                        <S.AnchorAdvantage href="/">VEJA COMO</S.AnchorAdvantage>
+                    </S.EachAdvantage>
+                </S.DivAllAdvantage>
+            </S.ContainerAdvantages>
         </>
     )
 }
---
+++ teslabank/src/styles/AdvantagesStyled.js
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+export const ContainerTitle = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 3rem;
+    margin-bottom: 1rem;
+`
+
+export const HashTag = styled.p`
+    margin-right: 57.3rem;
+    font-family: 'Sora', sans-serif;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 21.78px;
+    color: #E51D37;
+
+    @media(max-width: 1154px) {
+        align-items: center;
+        margin-right: 0rem;
+    }
+
+    @media(max-width: 600px) {
+        
+        margin-right: 12.4rem;
+    }
+`
+
+export const RedSpan = styled.span`
+    color: #E51D37;
+`
+
+export const Title = styled.h2`
+    margin-top: -1rem;
+    margin-right: 40rem;
+    font-family: 'Sora', sans-serif;
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 45.36px;
+    color: #333333;
+
+    @media(max-width: 1154px) {
+        align-items: center;
+        margin-right: 0rem;
+    }
+
+    @media(max-width: 600px) {
+        align-items: center;
+        font-size: 32px;
+        margin-right: 0rem;
+    }
+`
+
+export const ContainerAdvantages = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding-bottom: 2rem;
+    
+`
+
+export const DivAllAdvantage = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 2rem;
+
+    @media(max-width: 1154px) {
+        grid-template-columns: 1fr;
+    }
+`
+
+export const EachAdvantage = styled.div`
+    display: flex;
+    width: 260px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    padding: 2rem;
+    background-color: #ffff;
+    border-radius: 5px;
+`
+
+export const H3Advantage = styled.h3`
+    font-family: 'Sora', sans-serif;
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 32px;
+    letter-spacing: -0.014em;
+    text-align: left;
+
+`
+
+export const PAdvantage = styled.p`
+    font-size: 18px;
+    font-weight: 400;
+    margin-top: -0.7rem;
+    color: #868587;
+    text-align: justify;
+
+`
+
+export const AnchorAdvantage = styled.a`
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 32px;
+    letter-spacing: -0.014em;
+    text-align: left;
+    color: #E51D37;
+
+`
